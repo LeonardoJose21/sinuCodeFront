5ee97cc1796fb0424d98c86283fa547c8da6d5a6
Leonardo <90990794+LeonardoJose21@users.noreply.github.com>
2025-05-07 10:30:19 -0500

    Update api.js

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ import { ACCESS_TOKEN } from "./constants";
 // const apiUrl = "/choreo-apis/awbo/backend/rest-api-be2/v1.0";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: 'https://sinucode.onrender.com/'
 });
 
 api.interceptors.request.use(
