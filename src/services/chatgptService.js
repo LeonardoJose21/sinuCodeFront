6bd3d975b488b7532f26a2c77de76a9fd7f21186
Leonardo Pastrana <leopastra2004@gmail.com>
2025-05-08 01:24:26 -0500

    fix incorrect url

--- src/services/chatgptService.js
+++ src/services/chatgptService.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const chatgptApi = axios.create({
-    baseURL: 'http://localhost:8000/playground/api/chatgpt', // Adjust the URL as needed
+    baseURL: import.meta.env.VITE_API_URL+'playground/api/chatgpt', // Adjust the URL as needed
 });
 
 export const getChatGptResponse = async (input) => {
